5b871903f804c831ab767759a395ab0266a3cd13
Hafdekhil <202595642@collegeahuntsic.qc.ca>
2026-09-10 17:38:35 -0400

    feat(MEALSAVER-168): expose smart fridge route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,26 +13,28 @@ import { HouseholdPage } from "./pages/HouseholdPage";
 import { InventoryPage } from "./pages/InventoryPage";
 import { RecipesPage } from "./pages/RecipesPage";
 import { ScanPage } from "./pages/ScanPage";
+import { SmartFridgePage } from "./pages/SmartFridgePage";
 
 function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
         <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
+          <Route element={<Layout />}>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
 
-          <Route element={<ProtectedRoute />}>
-            <Route path="/household" element={<HouseholdPage />} />
-            <Route path="/inventory" element={<InventoryPage />} />
-            <Route path="/recipes" element={<RecipesPage />} />
-            <Route path="/scan" element={<ScanPage />} />
+            <Route element={<ProtectedRoute />}>
+              <Route path="/household" element={<HouseholdPage />} />
+              <Route path="/inventory" element={<InventoryPage />} />
+              <Route path="/recipes" element={<RecipesPage />} />
+              <Route path="/scan" element={<ScanPage />} />
+              <Route path="/smart-fridge" element={<SmartFridgePage />} />
+            </Route>
+
+            <Route path="*" element={<Navigate to="/" replace />} />
           </Route>
-
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Route>
         </Routes>
       </AuthProvider>
     </BrowserRouter>
